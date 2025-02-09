697dc91287646630ef93035f2a12b63d7bdd4aba
MaxineToTheStars <146043018+MaxineToTheStars@users.noreply.github.com>
2025-02-09 17:12:52 -0500

    Project Status Upload

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# libvoid
- A C memory management library
+<!-- Move text down -->
+<br>
+
+<!-- Header -->
+<h1 align="center">libVoid</h1>
+
+<!-- Subheading -->
+<h3 align="center">Practical data structures and utilities for C development</h3>
---
+++ include/arraylist.h
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2025 Astrl.
+ *
+ * This file is part of libVoid. It is subject to the license terms in
+ * the LICENSE file found in the top-level directory of this project and at
+ * https://github.com/The-Astrl-Project/libvoid/blob/HEAD/LICENSE.
+ *
+ * This file may not be copied, modified, propagated, or distributed
+ * except according to the terms contained in the LICENSE file.
+ */
+
+// Header Declarations
+// ----------------------------------------------------------------
+
+// ---
+
+// ---
+
+// ----------------------------------------------------------------
+
+// Macro Definitions
+
+// File Docstring
+// --------------------------------
+// libVoid || src/arraylist.c <-> include/arraylist.h
+//
+// An implementation of an ArrayList / Resizable Array.
+//
+// @author @MaxineToTheStars <https://github.com/MaxineToTheStars>
+// ----------------------------------------------------------------
+
+// Struct Definitions
+
+// Enum Definitions
+
+// Type Definitions
+
+// Variable Definitions
+
+// Main
+
+// Methods
